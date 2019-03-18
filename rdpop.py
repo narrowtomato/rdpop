16de03c61bf23c7a9c3584bfc3aefdca260a9770
Aaron Hahn <aaronhahn@candcadmins-MacBook-Pro.local>
2019-03-18 17:10:59 -0500

    Finished

---
+++ rdpop.py
@@ -0,0 +1,19 @@
+import pyautogui
+import time
+
+pyautogui.PAUSE = 0.3
+
+#read a list of server FQDNs from a text file
+serverFQDNs = open('servers.txt')
+
+#transfer control to Microsoft Remote Desktop
+
+pyautogui.hotkey('command', 'space')
+pyautogui.typewrite("Microsoft Remote Desktop")
+pyautogui.press('return')
+time.sleep(1)
+
+for server in serverFQDNs:
+    pyautogui.hotkey('command', 'n')
+    pyautogui.typewrite(server)
+    pyautogui.press('return')
