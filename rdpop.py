702ad54e022ffa4897ed93ebfe854bc2b3e330da
Aaron Hahn <aaronhahn@candcadmins-MacBook-Pro.local>
2019-03-21 09:30:54 -0500

    Added VNCPOP, support for populating VNC Viewer for MAC

--- rdpop.py
+++ rdpop.py
@@ -2,8 +2,8 @@ import pyautogui
 import os
 import time
 
-#set pyautogui to pause after every function call (otherwise it is too fast for RDP)
-pyautogui.PAUSE = 0.4
+#set pyautogui to pause after every function call (otherwise it is too fast)
+pyautogui.PAUSE = 0.5
 
 choice = ''
 
@@ -12,8 +12,9 @@ print('\n\n-----*****WELCOME TO RDPOP*****-----\n')
 while True:
     #display menu
     print("1. Run RDPOP")
-    print("2. Edit servers.txt")
-    print("3. Exit\n")
+    print("2. Run VNCPOP")
+    print("3. Edit servers.txt")
+    print("4. Exit\n")
 
     choice = input("Enter Selection: ")
 
@@ -24,8 +25,7 @@ while True:
         #transfer control to Microsoft Remote Desktop
 
         pyautogui.hotkey('command', 'space')
-        pyautogui.typewrite("Microsoft Remote Desktop")
-        pyautogui.press('return')
+        pyautogui.typewrite("Microsoft Remote Desktop\n")
         time.sleep(1)
 
         #loop through server FQDNs in the txt file and create RDP records for each
@@ -33,13 +33,29 @@ while True:
         for server in serverFQDNs:
             pyautogui.hotkey('command', 'n')
             pyautogui.typewrite(server)
-            pyautogui.press('return')
 
     elif choice == '2':
+        #read a list of server FQDNs from a text file
+        serverFQDNs = open('servers.txt')
+
+        #transfer control to VNC Viewer
+
+        pyautogui.hotkey('command', 'space')
+        pyautogui.typewrite("VNC Viewer")
+        pyautogui.press('return')
+        time.sleep(1)
+
+        #loop through server FQDNs in the txt file and create VNC records for each
+
+        for server in serverFQDNs:
+            pyautogui.hotkey('command', 'n')
+            pyautogui.typewrite(server)
+
+    elif choice == '3':
         #open servers.txt for editing
         os.system("vim servers.txt")
 
-    elif choice == '3':
+    elif choice == '4':
         exit()
 
     else:
